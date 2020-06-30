3863845ab0a5b001962745dfefff92170ea12a09
LucasFischer-max <66845046+LucasFischer-max@users.noreply.github.com>
2020-06-30 08:27:04 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -16,12 +16,9 @@ function Competicoes() {
 function Contato() {
     document.getElementById( 'Contato' ).scrollIntoView();    
 };
-
 var lastScrollTop = window.pageYOffset;
-
-// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
-window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
-   var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+window.addEventListener("scroll", function(){
+   var st = window.pageYOffset || document.documentElement.scrollTop;
    if (window.scrollY==0) {	
    	document.getElementById("menu").style.top = "0";
     document.getElementById("menu").style.backgroundColor = "#303030";
@@ -34,6 +31,6 @@ window.addEventListener("scroll", function(){ // or window.addEventListener("scr
       document.getElementById("menu").style.backgroundColor = "black";
    }
  }
- lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+ lastScrollTop = st <= 0 ? 0 : st;
 }, false);
    
