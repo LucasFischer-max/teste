728c2b98ef706eeae23a98b65c18200a0d28c3bc
LucasFischer-max <66845046+LucasFischer-max@users.noreply.github.com>
2020-07-09 17:12:04 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function openNav() {
-	document.getElementById("sidenav").style.width = "250px";
-}
+	document.getElementById("sidebar").style.width = "250px";
+	document.getElementById("menu").style.display = "none";
+};
 function closeNav() {
-	document.getElementById("sidenav").style.width = "0";
-}
+	document.getElementById("sidebar").style.width = "0";
+	document.getElementById("menu").style.display = "inline";
+};   
 function Sobre() {
     document.getElementById( 'Sobre' ).scrollIntoView();    
 };
@@ -15,6 +17,26 @@ function Competicoes() {
 };
 function Contato() {
     document.getElementById( 'Contato' ).scrollIntoView();    
+};
+function Sobre2() {
+    document.getElementById( 'Sobre' ).scrollIntoView();
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("menu").style.display = "inline";    
+};
+function Robos2() {
+    document.getElementById( 'Robos' ).scrollIntoView(); 
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("menu").style.display = "inline";
+};
+function Competicoes2() {
+    document.getElementById( 'Competicoes' ).scrollIntoView();
+    document.getElementById("sidebar").style.width = "0"; 
+    document.getElementById("menu").style.display = "inline"; 
+};
+function Contato2() {
+    document.getElementById( 'Contato' ).scrollIntoView(); 
+    document.getElementById("sidebar").style.width = "0"; 
+    document.getElementById("menu").style.display = "inline";  
 };
 var lastScrollTop = window.pageYOffset;
 window.addEventListener("scroll", function(){
