d8c35d14577ea9f7f587b5467f238626bdf85e27
LucasFischer-max <66845046+LucasFischer-max@users.noreply.github.com>
2020-07-12 13:05:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function openNav() {
 	document.getElementById("sidebar").style.width = "250px";
 	document.getElementById("menu").style.display = "none";
-};
+	document.getElementById("pagina").style.marginLeft = "250px";
+};	
 function closeNav() {
 	document.getElementById("sidebar").style.width = "0";
 	document.getElementById("menu").style.display = "inline";
+	document.getElementById("pagina").style.marginLeft = "0";
 };   
 function Sobre() {
     document.getElementById( 'Sobre' ).scrollIntoView();    
@@ -21,33 +23,37 @@ function Contato() {
 function Sobre2() {
     document.getElementById( 'Sobre' ).scrollIntoView();
     document.getElementById("sidebar").style.width = "0";
-    document.getElementById("menu").style.display = "inline";    
+    document.getElementById("menu").style.display = "inline"; 
+    document.getElementById("pagina").style.marginLeft = "0";   
 };
 function Robos2() {
     document.getElementById( 'Robos' ).scrollIntoView(); 
     document.getElementById("sidebar").style.width = "0";
     document.getElementById("menu").style.display = "inline";
+    document.getElementById("pagina").style.marginLeft = "0";
 };
 function Competicoes2() {
     document.getElementById( 'Competicoes' ).scrollIntoView();
     document.getElementById("sidebar").style.width = "0"; 
-    document.getElementById("menu").style.display = "inline"; 
+    document.getElementById("menu").style.display = "inline";
+    document.getElementById("pagina").style.marginLeft = "0"; 
 };
 function Contato2() {
     document.getElementById( 'Contato' ).scrollIntoView(); 
     document.getElementById("sidebar").style.width = "0"; 
-    document.getElementById("menu").style.display = "inline";  
+    document.getElementById("menu").style.display = "inline";
+    document.getElementById("pagina").style.marginLeft = "0";  
 };
 var lastScrollTop = window.pageYOffset;
 window.addEventListener("scroll", function(){
    var st = window.pageYOffset || document.documentElement.scrollTop;
    if (window.scrollY==0) {	
    	document.getElementById("menu").style.top = "0";
-    document.getElementById("menu").style.backgroundColor = "#303030";
+    document.getElementById("menu").style.backgroundColor = "#1e1e1e";
 }else {
 	if (st > lastScrollTop){
       document.getElementById("menu").style.top = "-55px";
-      document.getElementById("menu").style.backgroundColor = "#black";
+      document.getElementById("menu").style.backgroundColor = "black";
    } else {
       document.getElementById("menu").style.top = "0px";
       document.getElementById("menu").style.backgroundColor = "black";
